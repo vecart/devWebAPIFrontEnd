7484a84cbaad3e526d2835c4fd1ff01ccd0bea57
Scott Barona <vecartphoto@gmail.com>
2022-05-20 17:05:35 -0400

    added various click functionalities and corresponding modal functionality

--- src/app/department/department.component.ts
+++ src/app/department/department.component.ts
@@ -14,6 +14,10 @@ export class DepartmentComponent implements OnInit {
 
   departments: any=[];
 
+  modalTitle = "";
+  DepartmentId = 0;
+  DepartmentName = "";
+
   //LifeCycle method that is executed when component is in scope
   ngOnInit(): void {
     this.refreshList();
@@ -26,6 +30,53 @@ export class DepartmentComponent implements OnInit {
     .subscribe(data=>{
       this.departments=data;
     });
+  }
+
+  addClick(){
+    this.modalTitle="Add Department";
+    this.DepartmentId=0;
+    this.DepartmentName="";
+  }
+
+  editClick(dep:any){
+    this.modalTitle="Edit Department";
+    this.DepartmentId=dep.DepartmentId;
+    this.DepartmentName=dep.DepartmentName;
+  }
+
+  createClick(){
+    var val={
+      DepartmentName:this.DepartmentName
+    };
+
+    //POST Method to add new department
+    this.http.post(environment.API_URL+'department',val).subscribe(res=>{
+      alert(res.toString());
+      this.refreshList();
+    });
+  }
+
+  updateClick(){
+    var val={
+      DepartmentId:this.DepartmentId,
+      DepartmentName:this.DepartmentName
+    };
+
+    //PUT Method to update existing department based on given dept ID
+    this.http.put(environment.API_URL+'department',val).subscribe(res=>{
+      alert(res.toString());
+      this.refreshList();
+    });
+  }
+
+  deleteClick(id:any){
+    if(confirm('Are you sure?')){
+      //DELETE Method after confirmation
+      this.http.delete(environment.API_URL+'department/'+id).subscribe(res=>{
+        alert(res.toString());
+        this.refreshList();
+      });
+    }
   }
 
 }
